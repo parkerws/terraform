0d4fa874c016a891ca7762eaa4600f90c4c7d3e2
Will Parker <parkerws3824@gmail.com>
2022-03-23 12:01:56 -0400

    Update DCSetup.ps1

--- StarterLab/config/DCSetup.ps1
+++ StarterLab/config/DCSetup.ps1
@@ -10,3 +10,11 @@ choco install bginfo -y
 mkdir -Path "c:\LabSetup"
 mkdir -Path "c:\NTDS"
 
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/parkerws/terraform/master/StarterLab/config/DomainSetup.ps1" -OutFile "C:\LabSetup\DomainSetup.ps1"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/parkerws/terraform/master/StarterLab/config/DomainStructure.ps1" -OutFile "C:\LabSetup\DomainStructure.ps1"
+
+Set-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv4-In)" -Enabled True 
+Set-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv6-In)" -Enabled True
+
+Install-WindowsFeature -name Ad-Domain-Services -IncludeManagementTools
+Install-WindowsFeature -name DNS -IncludeManagementTools
